e6239cd35ff3ec218082cfba218b3c220c00f977
Petar Radosevic <petar@petar.dev>
2021-11-23 09:44:12 +0100

    feat: setup package management with straight

---
+++ init.el
@@ -0,0 +1,23 @@
+;; setup straight.el for package management
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+;; disable the package manager because we use straight.el
+(setq package-enable-at-startup nil)
+
+;; use-package for package management and configuration
+(straight-use-package 'use-package)
+
+;; Configure use-package to use straight.el by default
+(use-package straight
+         :custom (straight-use-package-by-default t))
