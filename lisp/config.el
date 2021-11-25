488a6e3402265c57ab187c4aab522c9d2e91e03f
Petar Radosevic <petar@petar.dev>
2021-11-25 09:33:08 +0100

    chore: update configuration according to Nano

--- lisp/config.el
+++ lisp/config.el
@@ -4,41 +4,106 @@
 
 (require 'lib)
 
-;; Make sure we always use UTF-8.
+;; Buffer encoding
+(prefer-coding-system       'utf-8)
+(set-default-coding-systems 'utf-8)
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
-(prefer-coding-system 'utf-8)
-(load-library "iso-transl")
+(set-language-environment   'utf-8)
 
-(setq inhibit-splash-screen t)      ; don't show the splash screen
-(setq initial-scratch-message nil)  ; don't show the scratch message
-(setq vc-follow-symlinks t)         ; just follow symlinks, don't ask me
-(defalias 'yes-or-no-p 'y-or-n-p)   ; ask for y/n instead of 'yes' or 'no'
-(setq ispell-program-name "aspell") ; use aspell for spell checking
-(setq sentence-end-double-space nil); double spaces do NOT end a sentence
-(setq ring-bell-function 'ignore)   ; no loud noises please
+;; No startup screen
+(setq inhibit-startup-screen t)
 
-;; Default modes
-(global-visual-line-mode 1)         ; let lines wrap
-(global-auto-revert-mode 1)         ; update the buffer when a file changes
-(delete-selection-mode 1)           ; delete marked region
+;; No startup message
+(setq inhibit-startup-message t)
+(setq inhibit-startup-echo-area-message t)
+
+;; No message in scratch buffer
+(setq initial-scratch-message nil)
+
+;; Initial buffer
+(setq initial-buffer-choice nil)
+
+;; No frame title
+(setq frame-title-format nil)
+
+;; No file dialog
+(setq use-file-dialog nil)
+
+;; No dialog box
+(setq use-dialog-box nil)
+
+;; No popup windows
+(setq pop-up-windows nil)
+
+;; No empty line indicators
+(setq indicate-empty-lines nil)
+
+;; Text mode is initial mode
+(setq initial-major-mode 'text-mode)
+
+;; Text mode is default major mode
+(setq default-major-mode 'text-mode)
+
+;; No cursor in inactive windows
+(setq cursor-in-non-selected-windows nil)
+
+;; Moderate font lock
+(setq font-lock-maximum-decoration nil)
+
+;; No limit on font lock
+(setq font-lock-maximum-size nil)
+
+;; No line break space points
+(setq auto-fill-mode nil)
+
+;; Fill column at 80
+(setq fill-column 80)
+
+;; No confirmation for visiting non-existent files
+(setq confirm-nonexistent-file-or-buffer nil)
+
+;; Completion style, see
+;; gnu.org/software/emacs/manual/html_node/emacs/Completion-Styles.html
+(setq completion-styles '(basic substring))
+
+;; Just follow symlinks
+(setq vc-follow-symlinks t)
+
+;; Ask for y/n instead of 'yes' or 'no'
+(defalias 'yes-or-no-p 'y-or-n-p)   
+
+;; Use aspell for spell checking
+(setq ispell-program-name "aspell")
+
+;; No load noises please
+(setq ring-bell-function 'ignore)
+
+;; Let lines wrap
+(global-visual-line-mode 1)
+
+;; Update the buffer when a file changes
+(global-auto-revert-mode 1)
+
+;; Delete a marked region
+(delete-selection-mode 1)
+
+;; Use shift + direction to switch between windows
+(windmove-default-keybindings)
 
 ;; Stop emacs from adding configuration to my setup
 (setq custom-file (make-temp-file "emacs-custom"))
 
 ;; Setup indentation
-(set-default 'indent-tabs-mode nil) ; we don't want to use tabs for indentation
-(setq-default tab-width 2)
-(setq-default js2-basic-offset 2)
-(setq-default js-indent-level 2)
-(setq-default py-indent-offset 2)
-(setq-default nxml-child-indent 2)
-(setq-default c-basic-offset 2)
-(setq-default web-mode-markup-indent-offset 2
-              web-mode-css-indent-offset 2
-              web-mode-code-indent-offset 2
-              web-mode-style-padding 2
-              web-mode-script-padding 2)
+(set-default 'indent-tabs-mode nil)
+(setq-default tab-width 4)
+
+;; Size of temporary buffers
+(temp-buffer-resize-mode)
+(setq temp-buffer-max-height 8)
+
+;; Minimum window height
+(setq window-min-height 1)
 
 ;; Emacs writes backup files to `filename~` by default. This is messy,
 ;; so let's tell it to write them to `~/.emacs.d/backup` instead.
@@ -56,9 +121,6 @@
   (setq-default
    browse-url-browser-function (quote browse-url-generic)
    browse-url-generic-program "xdg-open"))
-
-;; aspell for spell-checking
-
 
 (provide 'config)
 ;;; config.el ends here
