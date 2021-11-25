6cc12ba5907166a7d5eb7606145585b31655b79a
Petar Radosevic <petar@petar.dev>
2021-11-25 11:17:37 +0100

    feat: detect system

--- lisp/lib.el
+++ lisp/lib.el
@@ -23,5 +23,20 @@
   "Returns true if `command' is an executable on the system search path"
   (f-executable? (s-trim (shell-command-to-string (s-concat "which " command)))))
 
+(defun is-mac ()
+  "Return non-nil if running on a mac."
+  (interactive)
+  (string-equal system-type "darwin"))
+
+(defun is-windows ()
+  "Returns true if running on Windows WSL"
+  (interactive)
+  (string-match "-[Mm]icrosoft" operating-system-release))
+
+(defun is-linux ()
+  "Returns true if running on native Linux"
+  (interactive)
+  (and (not (is-windows)) (string-equal system-type "gnu/linux")))
+
 (provide 'lib)
 ;;; lib.el ends here
