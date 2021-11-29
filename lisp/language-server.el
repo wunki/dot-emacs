1bc47571a329c810747b2c90a15ce98aa0ca21af
Petar Radosevic <petar@petar.dev>
2021-11-29 11:25:05 +0100

    feat: setup LSP for clojure

--- lisp/language-server.el
+++ lisp/language-server.el
@@ -4,8 +4,15 @@
 
 (use-package lsp-mode
   :commands lsp
+  :config
+  (setq lsp-lens-enable t)
+  (setq lsp-signature-auto-activate nil)
+  (setq lsp-headerline-breadcrumb-enable nil)
+  (setq lsp-semantic-tokens-mode t)
   :hook
-  (lsp-mode . lsp-enable-which-key-integration))
+  ((before-save . lsp-format-buffer)
+   (before-save . lsp-organize-imports)
+   (lsp-mode . lsp-enable-which-key-integration)))
 
 (use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
 (use-package lsp-treemacs :commands lsp-treemacs-errors-list)
--- lisp/languages.el
+++ lisp/languages.el
@@ -2,6 +2,13 @@
 
 ;;; Code:
 
+;; Clojure
+(use-package clojure-mode)
+
+(use-package cider
+  :config
+  ;; We use clojure-lsp for showing documentation
+  (setq cider-eldoc-display-for-symbol-at-point nil))
 
 (provide 'languages)
 ;;; languages.el ends here
