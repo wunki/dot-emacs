bf44cb7679c704d027aab81cfcc23d89076fa127
Petar Radosevic <petar@petar.dev>
2021-12-29 21:56:20 +0100

    languages: remove clojure as seperate library

--- init.el
+++ init.el
@@ -2,8 +2,8 @@
 ;; Add my configuration directory to the load path
 (add-to-list 'load-path (concat user-emacs-directory "lisp"))
 
-(require 'config)
 (require 'packages)
+(require 'config)
 (require 'lib)
 (require 'bindings)
 (require 'navigation)
@@ -15,6 +15,3 @@
 
 (if (is-linux)
   (require 'mail))
-
-;; Languages
-(require 'clojure)
--- lisp/clojure.el
+++
@@ -1,8 +0,0 @@
-;;; clojure.el --- Setup Clojure environment -*- lexical-binding: t -*-
-
-;;; Code:
-
-
-
-(provide 'clojure)
-;;; clojure.el ends here
