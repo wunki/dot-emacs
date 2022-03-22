084481106b7b35e023c63e8e0d90c7996eacb567
Petar Radosevic <petar@petar.dev>
2022-03-22 11:04:55 +0100

    feat: quickly start and close the eshell

--- lisp/lib.el
+++ lisp/lib.el
@@ -61,5 +61,29 @@
           (message "Deleted file %s" filename)
           (kill-buffer))))))
 
+(defun pet/eshell-here ()
+  "Opens up a new shell in the directory associated with the current buffer's file.
+The eshell is renamed to match that directory to make multiple eshell
+windows easier."
+  (interactive)
+  (let*
+    (
+      (parent
+        (if (buffer-file-name)
+          (file-name-directory (buffer-file-name))
+          default-directory))
+      (height (/ (window-total-height) 3))
+      (name (car (last (split-string parent "/" t)))))
+    (split-window-vertically (- height))
+    (other-window 1)
+    (eshell "new")
+    (rename-buffer (concat "*eshell: " name "*"))))
+(global-set-key (kbd "C-c s") 'pet/eshell-here)
+
+(defun eshell/q ()
+  (insert "exit")
+  (eshell-send-input)
+  (delete-window))
+
 (provide 'lib)
 ;;; lib.el ends here
