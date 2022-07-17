f8a53aab71d818764346bb683d3b7677b2fe810a
Petar Radosevic <petar@petar.dev>
2022-07-17 08:28:40 +0200

    Mindre theme

--- lisp/looks.el
+++ lisp/looks.el
@@ -35,7 +35,7 @@
 (use-package modus-themes
   :commands (modus-themes-load-themes modus-themes-load-operandi)
   :init
-  (setq modus-themes-mode-line '(3d accented borderless (padding . 4))
+  (setq modus-themes-mode-line '(accented borderless (padding . 4))
         modus-themes-italic-constructs t
         modus-themes-bold-constructs nil
         modus-themes-region '(bg-only no-extend)
@@ -46,8 +46,17 @@
         modus-themes-region '(bg-only))
   (modus-themes-load-themes)
   :config
-  (modus-themes-load-operandi)
+  ;; (modus-themes-load-operandi)
   :bind ("<f5>" . modus-themes-toggle))
+
+(use-package mindre-theme
+    :ensure t
+    :straight (:host github :repo "erikbackman/mindre-theme")
+    :custom
+    (mindre-use-more-bold nil)
+    (mindre-use-faded-lisp-parens t)
+    :config
+    (load-theme 'mindre t))
 
 (use-package fontset
   :straight (:type built-in)
