192974e7997c8d370a6fb003c6170c1a14a6a4c1
Petar Radosevic <petar@petar.dev>
2023-01-07 15:44:15 +0100

    chore: back to my own theme

--- lisp/looks.el
+++ lisp/looks.el
@@ -43,25 +43,6 @@
   :ensure t
   :if (display-graphic-p))
 
-(use-package doom-themes
-  :ensure t
-  :config
-  ;; Global settings (defaults)
-  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-        doom-themes-enable-italic t) ; if nil, italics is universally disabled
-
-  (load-theme 'doom-homage-white t)
-
-  ;; Enable flashing mode-line on errors
-  (doom-themes-visual-bell-config)
-  ;; Enable custom neotree theme (all-the-icons must be installed!)
-  (doom-themes-neotree-config)
-  ;; or for treemacs users
-  (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
-  (doom-themes-treemacs-config)
-  ;; Corrects (and improves) org-mode's native fontification.
-  (doom-themes-org-config))
-
 (use-package doom-modeline
   :ensure t
   :hook (after-init . doom-modeline-mode))
@@ -72,7 +53,7 @@
   :demand t
   :ensure t
   :config
-  ; (load-theme 'stimmung-themes-light t)
+  (load-theme 'stimmung-themes-light t)
   )
 
 (use-package modus-themes
