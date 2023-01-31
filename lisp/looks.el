0859f93a8540bfb7b21727c7605b80d659534a90
Petar Radosevic <petar@petar.dev>
2023-01-31 10:53:29 +0100

    change the looks

--- lisp/looks.el
+++ lisp/looks.el
@@ -22,7 +22,7 @@
    ((pet/is-bsd) "Triplicate T4 10")
    ((pet/is-linux) "Triplicate T4 12")
    ((pet/is-wsl) "Triplicate T4 17")
-   ((pet/is-mac) "IBM Plex Mono 13")))
+   ((pet/is-mac) "MonoLisa 13")))
 (pet/set-font petars-font)
 
 ;; Don't show any bars or toolbars
@@ -54,32 +54,12 @@
   (doom-modeline-height 25)
   :hook (after-init . doom-modeline-mode))
 
-(use-package stimmung-themes
-  :straight (stimmung-themes :host github :repo "motform/stimmung-themes")
-  :commands stimmung-themes-load-light
-  :demand t
-  :ensure t
-  :config
-  (load-theme 'stimmung-themes-light t))
-
-(use-package modus-themes
-  :ensure
-  :commands (modus-themes-load-themes modus-themes-load-vivendi)
-  :custom
-  (modus-themes-italic-constructs t)
-  (modus-themes-bold-constructs t)
-  (modus-themes-region '(bg-only no-extend))
-  (modus-themes-syntax '(faint yellow-comments))
-  (modus-themes-mode-line '(accented borderless (padding . 4) (height . 0.9)))
-  (modus-themes-paren-match '(bold))
-  :init
-  ;; (modus-themes-load-themes)
-  :config
-  ;; (modus-themes-load-vivendi)
-  :bind ("C-c C-t" . modus-themes-toggle))
-
 (use-package ef-themes
-  :ensure)
+  :ensure
+  :commands ef-themes-select
+  :bind ("C-c C-t" . ef-themes-load-random)
+  :config
+  (ef-themes-select 'ef-tritanopia-dark))
 
 (use-package rainbow-mode
   :commands rainbow-mode
