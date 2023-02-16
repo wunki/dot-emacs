c977a5cb0ed4df865067eab4affa4c9de0051a84
Petar Radosevic <petar@petar.dev>
2023-02-16 10:29:06 +0100

    fix: improve font setting

--- lisp/lib.el
+++ lisp/lib.el
@@ -117,7 +117,7 @@ If the window-system is active, it directly changes the font.
 Otherwise it adds it to the so it works with the Emacs daemon."
   (interactive "sFont: ")
   (if (window-system)
-      (set-frame-font font)
+      (set-frame-font font nil t)
     (add-to-list 'default-frame-alist `(font . ,font))))
 
 (provide 'lib)
