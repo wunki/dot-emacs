3b9a9dbe3d7441bb62793f79e183e5832df067bb
Petar Radosevic <petar@petar.dev>
2023-02-20 09:21:59 +0100

    setup language server for java

--- lisp/language-server.el
+++ lisp/language-server.el
@@ -10,7 +10,7 @@
 (require 'lib)
 
 (use-package eglot
-  :hook (((zig-mode elixir-mode c-mode) . eglot-ensure)
+  :hook (((zig-mode elixir-mode c-mode java-ts-mode) . eglot-ensure)
          (before-save . eglot-format-buffer))
   :config
   (setq eglot-autoshutdown t
@@ -20,6 +20,7 @@
         eglot-send-changes-idle-time 0.5
         eglot-ignored-server-capabilities '(:hoverProvider))
   (add-to-list 'eglot-server-programs '(elixir-mode "~/.local/share/elixir-ls/release/language_server.sh"))
+  (add-to-list 'eglot-server-programs '(java-ts-mode "jdtls"))
   (add-to-list 'eglot-server-programs '(c-mode "clangd")))
 
 (provide 'language-server)
