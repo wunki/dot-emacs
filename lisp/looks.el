bd31e8fe4652c401652ff9c01230e0aa593e1001
Petar Radosevic <petar@petar.dev>
2023-03-31 11:33:57 +0200

    chore: ibm plex mono

--- lisp/looks.el
+++ lisp/looks.el
@@ -18,7 +18,7 @@
 (defvar petars-font
   (cond
    ((pet/is-bsd) "Triplicate T4 10")
-   ((pet/is-linux) "MonoLisa 8")
+   ((pet/is-linux) "IBM Plex Mono 9")
    ((pet/is-wsl) "MonoLisa 16")
    ((pet/is-mac) "MonoLisa 13")))
 (pet/set-font petars-font)
