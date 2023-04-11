7de694862941d9ea640e5c2f516b748a9d7ee892
Petar Radosevic <petar@petar.dev>
2023-04-11 06:36:28 +0200

    chore: litte bit smaller fonts

--- lisp/looks.el
+++ lisp/looks.el
@@ -18,7 +18,7 @@
 (defvar petars-font
   (cond
    ((pet/is-bsd) "Triplicate T4 10")
-   ((pet/is-linux) "IBM Plex Mono 10")
+   ((pet/is-linux) "IBM Plex Mono 9")
    ((pet/is-wsl) "MonoLisa 16")
    ((pet/is-mac) "IBM Plex Mono 14")))
 (pet/set-font petars-font)
