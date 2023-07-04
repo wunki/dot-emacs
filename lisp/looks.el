613b5304927e83c4b6be5cf3992a451a88a9fd2e
Petar Radosevic <petar@petar.ai>
2023-07-04 09:36:22 +0200

    massively simplify the looks

--- lisp/looks.el
+++ lisp/looks.el
@@ -3,10 +3,18 @@
 ;;; Commentary:
 ;;
 ;; We want Emacs to look nice, with as minimal distraction as possible.
+;; That's why we use the Nano theme from rougier.
 ;; 
 ;;; Code:
 ;;
 (require 'lib)
+
+(straight-use-package
+  '(nano-emacs :type git :host github :repo "rougier/nano-emacs"))
+
+(setq nano-font-family-monospaced "Berkeley Mono")
+(setq nano-font-size 14)
+(require 'nano)
 
 ;; Easily scale the font size up and down
 (use-package default-text-scale
@@ -14,92 +22,11 @@
   :commands default-text-scale-mode
   :config (default-text-scale-mode))
 
-;; Set the font, depending on the system
-(defvar petars-font
-  (cond
-   ((pet/is-bsd) "Triplicate T4 10")
-   ((pet/is-linux) "IBM Plex Mono 9")
-   ((pet/is-wsl) "IBM Plex Mono 15")
-   ((pet/is-mac) "Berkeley Mono 17")))
-(pet/set-font petars-font)
-
-;; Don't show any bars or toolbars
-(dolist (mode '(menu-bar-mode tool-bar-mode scroll-bar-mode))
-  (when (fboundp mode)
-    (funcall mode 0)))
-
-;; Add some spacing
-(setq frame-resize-pixelwise t
-      default-frame-alist (append (list
-                                   '(vertical-scroll-bars . nil)
-                                   '(internal-border-width . 14)
-                                   '(right-fringe   . 0)
-                                   '(tool-bar-lines . 0))))
-
-(when (memq window-system '(mac ns))
-  (add-to-list 'default-frame-alist '(ns-appearance . nil))
-  (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t)))
-
-;; load my own themes
-(use-package autothemer
-  :disabled
-  :init
-  (add-to-list
-   'custom-theme-load-path
-   (concat user-emacs-directory "themes/"))
-  :config
-  (load-theme 'kanagawa t))
-
-(use-package all-the-icons
-  :ensure t
-  :if (display-graphic-p))
-
-(use-package doom-themes
-  :config
-  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-        doom-themes-enable-italic t)   ; if nil, italics is universally disabled
-  (load-theme 'doom-homage-white t)
-  (setq doom-themes-treemacs-theme "doom-atom")
-  (doom-themes-treemacs-config)
-  (doom-themes-org-config))
-
-(use-package doom-modeline
-  :ensure t
-  :custom
-  (doom-modeline-icon nil)
-  (doom-modeline-buffer-encoding nil)
-  (doom-modeline-height 25)
-  :hook (after-init . doom-modeline-mode))
-
-(use-package modus-themes
-  :disabled
-  :ensure t
-  :bind ("C-c C-t" . modus-themes-toggle)
-  :config
-  (setq modus-themes-italic-constructs t
-        modus-themes-bold-constructs nil)
-  :init
-  (load-theme 'modus-vivendi-tinted t))
-
-(use-package ef-themes
-  :disabled
-  :ensure t
-  :commands ef-themes-select
-  :bind ("C-c C-t" . ef-themes-load-random)
-  :init
-  (ef-themes-select 'ef-deuteranopia-dark)
-  :custom
-  (ef-themes-region '(intense no-extend neutral)))
-
 (use-package rainbow-mode
   :commands rainbow-mode
   :diminish
   :hook ((web-mode . rainbow-mode)
          (css-mode . rainbow-mode)))
-
-(use-package emojify
-  :hook (erc-mode . emojify-mode)
-  :commands emojify-mode)
 
 (provide 'looks)
 ;;; looks.el ends here
