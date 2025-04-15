8cad9a1238b2e2a3ddeb89b554e57ca454e303d3
Petar Radosevic <petar@petar.dev>
2025-04-15 12:22:27 +0200

    increase internal border

--- lisp/pet-looks.el
+++ lisp/pet-looks.el
@@ -50,7 +50,7 @@
 (setq frame-resize-pixelwise t
       default-frame-alist (append (list
                                    '(vertical-scroll-bars . nil)
-                                   '(internal-border-width . 0)
+                                   '(internal-border-width . 10)
                                    '(right-fringe   . 0)
                                    '(tool-bar-lines . 0))))
 
