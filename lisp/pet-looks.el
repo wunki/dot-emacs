0285dd9d21892323a32041a141a74bdaa74e0d3d
Petar Radosevic <petar@petar.dev>
2026-05-06 07:28:04 +0200

    back to space mono

--- lisp/pet-looks.el
+++ lisp/pet-looks.el
@@ -12,9 +12,9 @@
   (setq-default fontaine-presets
                 '((regular)
                   (default
-                   :default-family "CommitMono"
+                   :default-family "Space Mono"
                    :default-height 120
-                   :line-spacing 0.3)
+                   :line-spacing 0.1)
                   (t
                    :default-family "Maple Mono NF"
                    :default-weight regular
