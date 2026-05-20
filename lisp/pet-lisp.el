604345100c2508daed6e05f19c2b18b196504063
Petar Radosevic <petar@petar.dev>
2026-05-20 08:20:19 +0200

    lisp deserves its own config

---
+++ lisp/pet-lisp.el
@@ -0,0 +1,77 @@
+;;; pet-lisp.el --- Common Lisp configuration -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+(require 'pet-lib)
+
+;; projects don't need to be added to Git to be found
+(defun pet/project-find-asdf-system (dir)
+  "Find Common Lisp ASDF project root for DIR."
+  (when-let* ((root (locate-dominating-file
+                     dir
+                     (lambda (directory)
+                       (directory-files directory nil "\\.asd\\'")))))
+    (cons 'asdf-system root)))
+
+(cl-defmethod project-root ((project (head asdf-system)))
+  "Return root of ASDF system PROJECT."
+  (cdr project))
+
+(add-hook 'project-find-functions #'pet/project-find-asdf-system)
+
+(use-package paredit
+  :hook (lisp-mode . paredit-mode))
+
+;; open up Hyperspec urls in Emacs itself
+(use-feature browse-url
+  :config
+  (add-to-list
+   'browse-url-handlers
+   '("https://www\\.lispworks\\.com/reference/HyperSpec/" . eww-browse-url)))
+
+(use-package sly
+  :init
+  (setq sly-contribs '(sly-fancy
+                       sly-asdf
+                       sly-profiler
+                       sly-trace-dialog
+                       sly-stickers))
+  :custom
+  (inferior-lisp-program "sbcl")
+  (sly-lisp-implementations '((sbcl ("sbcl"))))
+  :bind (:map sly-mode-map
+              ("C-c C-z" . pet/sly-mrepl-toggle)
+              ("C-c C-k" . sly-compile-and-load-file)
+              ("C-c C-r" . sly-compile-region)
+              ("C-c C-d d" . sly-describe-symbol)
+              ("C-c C-d h" . sly-hyperspec-lookup))
+  :preface
+  (defvar pet/sly-source-buffer nil
+    "Buffer to return to from the SLY REPL.")
+  (defun pet/sly-mrepl-toggle ()
+    "Toggle between source buffer and the SLY REPL."
+    (interactive)
+    (if (derived-mode-p 'sly-mrepl-mode)
+        (if (buffer-live-p pet/sly-source-buffer)
+            (pop-to-buffer pet/sly-source-buffer)
+          (message "No source buffer remembered"))
+      (setq pet/sly-source-buffer (current-buffer))
+      (call-interactively #'sly-mrepl))))
+
+(use-package sly-mrepl
+  :ensure nil
+  :after sly
+  :custom
+  (sly-mrepl-history-file-name
+   (no-littering-expand-var-file-name "sly/mrepl-history"))
+  (sly-mrepl-prevent-duplicate-history 'move)
+  :bind (:map sly-mrepl-mode-map
+              ("C-c C-z" . pet/sly-mrepl-toggle)))
+
+(use-package sly-asdf
+  :after sly
+  :config
+  (add-to-list 'sly-contribs 'sly-asdf 'append))
+
+(provide 'pet-lisp)
+;;; pet-lisp.el ends here
