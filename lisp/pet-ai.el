dcee2741dd21c2aece85ae1d28bc8ef824f7c671
Petar Radosevic <petar@petar.dev>
2026-08-02 08:04:16 +0200

    fix gptel

--- lisp/pet-ai.el
+++ lisp/pet-ai.el
@@ -4,14 +4,15 @@
 
 (use-package gptel
   :functions (gptel-make-openai-oauth)
-  :defines (gptel-backend gptel-model)
+  :defines (gptel-backend gptel-model gptel-openai-oauth-login-method)
   :commands (gptel gptel-send gptel-menu gptel-rewrite gptel-add)
   :bind (("C-c a" . gptel-add)
          ("C-c i" . gptel-menu))
   :init
-  (setq gptel-model 'gpt-5.5)
+  (setq gptel-model 'gpt-5.6-sol)
   :config
-  (setq gptel-backend (gptel-make-openai-oauth "OpenAI-subscription"))
+  (setq gptel-backend (gptel-make-openai-oauth "OpenAI-sub"))
+  (setq gptel-openai-oauth-login-method 'device)
   ;; disable the ugly background when things are added
   ;; to the context
   (with-eval-after-load 'gptel-context
